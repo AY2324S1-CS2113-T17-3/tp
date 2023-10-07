e934e05174f1163937686bead0f67e8bd584a94f
TeoHaoZhi <e0957449@u.nus.edu>
2023-10-08 00:10:35 +0800

    Add drug class attributes

---
+++ src/main/java/seedu/stocker/drugs/Drug.java
@@ -0,0 +1,8 @@
+package seedu.stocker.drugs;
+
+public class Drug {
+
+    String name;
+    String expiryDate;
+    Long quantity;
+}
--- src/main/java/seedu/stocker/drugs/Drugs.java
+++
@@ -1,4 +0,0 @@
-package seedu.stocker.drugs;
-
-public class Drugs {
-}
